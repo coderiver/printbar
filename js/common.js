4871ea75c9150945662dd6985ade0b129f3890b5
Valera19900 <kaktuz34@gmail.com>
2014-07-14 18:03:31 +0300

    index

--- js/common.js
+++ js/common.js
@@ -4,5 +4,43 @@ head.ready(function() {
 	// 	$(".js-popup").hide();
 	// });
 
-	console.log($('body').html());
+	//tab
+	function tab() {
+			$(".js-tab").each(function(){
+				var tab_link = $(this).find("a");
+				var tab_item = $(this).find("li");
+				var tab_cont = $(this).parents(".js-tab-group").find(".js-tab-cont");
+				tab_cont.hide();
+				tab_item.first().addClass("is-active");
+				$(this).parents(".js-tab-group").find(".js-tab1").show();
+				tab_link.on("click", function() {
+					var index = $(this).attr("href");
+					tab_item.removeClass("is-active");
+					$(this).parent().addClass("is-active");
+					tab_cont.hide();
+					$(this).parents(".js-tab-group").find("."+index).show();
+					return false;
+				});
+			});
+	}
+	tab();
+
+	$('.responsive').slick({
+		dots: true,
+		infinite: true,
+		speed: 300,
+		slidesToShow: 5,
+		slidesToScroll: 1,
+		responsive: [
+		{
+			breakpoint: 1480,
+			settings: {
+				slidesToShow: 4,
+				slidesToScroll: 1,
+				infinite: true,
+				dots: true
+			}
+		}
+		]
+	});
 });
